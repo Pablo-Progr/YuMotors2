3ea1fed4946d677dc3ed81b9372ba5518d45324a
Pablo <dominguezpg.99@gmail.com>
2025-11-02 20:41:45 -0300

    logica para obtener accesorios backend

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const loginRoutes = require('./login.routes');
 const repuestosRoutes = require('./repuestos.routes');
+const accesoriosRoutes = require('./accesorios.routes');
 
 const router = express.Router();
 
 
 router.use('/admin', loginRoutes);
 router.use('/repuestos', repuestosRoutes);
+router.use('/accesorios', accesoriosRoutes);
 
 module.exports = router;
