937f7ddadf5eb02e11d330f5d218265480a0755b
GiulianoArk <calcagnigiuliano770@gmail.com>
2025-11-05 15:20:50 -0300

    Se empezó con la página de los autos usados

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import RepuestosAdmin from "./pages/RepuestosAdmin";
 import VehiculosAdmin from "./pages/VehiculosAdmin";
 import ConsultasAdmin from "./pages/ConsultasAdmin";
 import Home from "./pages/Home";
+import Modelos from "./pages/Modelos";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/modelos" element={<Modelos />} />
           <Route path="/login" element={<AdminLogin />} />
           <Route path="/admin/accesorios" element={<AccesoriosAdmin />} />
           <Route path="/admin/repuestos" element={<RepuestosAdmin />} />
---
+++ frontend/src/pages/Modelos.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+import MainModelos from '../components/MainModelos'
+import Footer from '../components/Footer'
+const Modelos = () => {
+  return (
+    <div>
+        <Header />
+        <MainModelos />
+        <Footer />
+    </div>
+  )
+}
+
+export default Modelos
