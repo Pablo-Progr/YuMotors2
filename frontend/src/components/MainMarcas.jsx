b6facb3d8f4c95d0a46f1df494078054e31bf379
GiulianoArk <calcagnigiuliano770@gmail.com>
2025-11-08 21:35:51 -0300

    Creada la sección para elegir una marca

--- frontend/src/components/MainMarcas.jsx
+++ frontend/src/components/MainMarcas.jsx
@@ -1,24 +1,42 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import lexus from "../img/lexus-logo.png";
+import gr from "../img/grlogo.png";
+import toyota from "../img/toyota.png";
+import "../css/marcas.css";
 
 const MainMarcas = () => {
   return (
-    <div>
-      <div className='card-container'>
-        <div className='card'> 
-          <h5>Marca 1</h5>
-          <Link to="/marcas/toyota" className="btn">Toyota</Link>
-        </div>
-        <div className='card'> 
-          <h5>Marca 2</h5>
-          <Link to="/marcas/gr" className="btn">Gazoo Racing</Link>
-        </div>
-        <div className='card'> 
-          <h5>Marca 3</h5>
-          <Link to="/marcas/lexus" className="btn">Lexus</Link>
-        </div>
-      </div>
-    </div>
-  )
-}
+    <div className="MainMarcas">
+      <div className="card-container-marcas">
 
-export default MainMarcas
+        <div className="card-marcas">
+          <Link to="/marcas/toyota" className="btn">
+            <img src={toyota} alt="toyota" />
+          </Link>
+        </div>
+
+        <div className="card-marcas">
+          <Link to="/marcas/gr" className="btn">
+            <img src={gr} alt="Gazoo Racing" />
+          </Link>
+        </div>
+
+        <div className="card-marcas">
+          <Link to="/marcas/lexus" className="btn">
+            <img src={lexus} alt="Lexus" />
+          </Link>
+        </div>
+
+      </div>
+
+      <div className="btn-volver-container">
+        <Link to="/" className="btn">
+          <button className="btn-volver">Volver</button>
+        </Link>
+      </div>
+
+    </div>
+  );
+};
+
+export default MainMarcas;
