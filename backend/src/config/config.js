46f4ae3af3948a86e78047835e9bc3d635476330
Pablogd <dominguezpg.99@gmail.com>
2025-11-10 14:48:20 -0300

    a

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 dotenv.config();
 
 const db = mysql.createConnection({
-    host: process.env.db_host,
+    host: process.env.db_host, 
     port: process.env.db_port,
     database: process.env.db_name,
     user: process.env.db_user,
